0eb92c51a417e2d09e2d94e2f064cd2d8ca11d36
gghatano <adad.0405@gmail.com>
2021-09-29 14:13:46 +0900

    check-result

--- check_result.py
+++ check_result.py
@@ -1,6 +1,6 @@
 ## 推定結果の正解判定を行う関数
 
-import pandas as pd
+import pandas as pd 
 import numpy as np
 
 def check_result(dat_e : pd.DataFrame, target_team_id : str) -> pd.DataFrame:
@@ -59,21 +59,4 @@ def check_result(dat_e : pd.DataFrame, target_team_id : str) -> pd.DataFrame:
         dat_e["REID_ESTIMATION"] = 0
 
   return(dat_e)
-
-
-## 推定結果を読み込み、membership, precision, reacallを表示するだけ
-
-def print_estimation_result(dat_e_a: pd.DataFrame):
-  '''print_estimation_result
-    推定結果を表示する membership, precision, recall
-  '''
-
-  membership = np.mean(dat_e_a["MEMBERSHIP_ESTIMATION"])
-  precision = 0; ## TODO
-  recall = 0; ## TODO
-
-  print("Membership: " + str(membership))
-  print("Precision: " + str(precision))
-  print("Recall: " + str(recall))
-
 
